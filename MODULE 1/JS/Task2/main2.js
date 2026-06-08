d10e34ae9f48130a6fb52059638b89440416eae2
Zeeta2 <felijanezeewin@gmail.com>
2026-06-08 21:41:53 +0530

    Add files via upload

---
+++ MODULE 1/JS/Task2/main2.js
@@ -0,0 +1,9 @@
+const eventName = "Community Meetup";
+const eventDate = "15-06-2026";
+let seats = 50;
+
+console.log(`Event: ${eventName}, Date: ${eventDate}, Available Seats: ${seats}`);
+
+seats--;
+
+console.log(`Available Seats after Registration: ${seats}`);
